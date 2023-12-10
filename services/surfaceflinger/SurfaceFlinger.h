3db461026de946ab585c4cd8a5d786fe09bb3214
El Dainosor <eldainosor@gmail.com>
2023-12-10 17:23:12 -0300

    Revert "SF: Bring back support for disabling backpressure propagation"
    reason: conflicts with upstream changes
    
    This reverts commit 2a3595d9144842c7c15c737641355f5e69dad01f.

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -1254,7 +1254,6 @@ private:
 
     bool mLayerCachingEnabled = false;
     bool mBackpressureGpuComposition = false;
-    bool mPropagateBackpressure = true;
 
     LayerTracing mLayerTracing;
     bool mLayerTracingEnabled = false;
